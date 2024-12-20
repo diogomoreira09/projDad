8e88cf347b5f73604ff64c15d637e9282b8ebc72
Ricardo Oiteiro <2201766@my.ipleiria.pt>
2024-12-20 23:59:47 +0000

    ficheiro para o deploy

--- game_vue_client/src/App.vue
+++ game_vue_client/src/App.vue
@@ -136,7 +136,7 @@ const handleMessageFromInputDialog = (message) => {
         class="w-24 h-10 leading-10 text-center rounded-t-xl border-none text-white select-none bg-gray-400 cursor-pointer hover:bg-gray-500"
         active-class="bg-gray-800 hover:bg-gray-800"
       >
-        Regiter
+        Register
       </RouterLink>
 
       <RouterLink
